a2304685becca71908fbd2a77eb88679de0f5f68
Chris Blakely <chrisblakely@Chriss-MacBook-Pro.local>
2018-12-26 21:17:53 +0000

    Completed basic board tiles minus some stlying. Added titles to board center elements

--- src/Board.js
+++ src/Board.js
@@ -1,73 +1,21 @@
 import React, { Component } from 'react';
 import './Board.css';
-import Property from './Property';
+import BottomSide from './BottomSide';
+import LeftSide from './LeftSide';
+import TopSide from './TopSide';
+import BoardCenter from './BoardCenter'
+import RightSide from './RightSide';
 
 class Board extends Component {
 	render() {
 		return (
 			<div class="container-main">
 				<div class="board">
-					<div class="center">
-						<div class="chance-deck"></div>
-						<div class="community-deck"></div>
-					</div>
-					<div class="bottom-tile bottom-row-1">
-						<div class="go-tile">
-							<div class="go-subtext">Pass go & Collect £200</div>
-							<div class="go-text">GO</div>
-						</div>
-					</div>
-					<div class="bottom-tile bottom-row-2">
-						<Property color="brown" name="Mediterranean Avenue" cost="60" />
-					</div>
-					<div class="bottom-tile bottom-row-3">
-					</div>
-					<div class="bottom-tile bottom-row-4">
-						<Property color="brown" name="Baltic Avenue" cost="60" />
-					</div>
-					<div class="bottom-tile bottom-row-5"></div>
-					<div class="bottom-tile bottom-row-6"></div>
-					<div class="bottom-tile bottom-row-7">
-						<Property color="light-blue" name="Oriental Avenue" cost="100" />
-					</div>
-					<div class="bottom-tile bottom-row-8"></div>
-					<div class="bottom-tile bottom-row-9">
-						<Property color="light-blue" name="Vermont Avenue" cost="100" />
-					</div>
-					<div class="bottom-tile bottom-row-10">
-						<Property color="light-blue" name="Connecicut Avenue" cost="120" />
-					</div>
-					<div class="left-tile left-row-1"></div>
-					<div class="left-tile left-row-2">
-					</div>
-					<div class="left-tile left-row-3"></div>
-					<div class="left-tile left-row-4"></div>
-					<div class="left-tile left-row-5"></div>
-					<div class="left-tile left-row-6"></div>
-					<div class="left-tile left-row-7"></div>
-					<div class="left-tile left-row-8"></div>
-					<div class="left-tile left-row-9"></div>
-					<div class="left-tile left-row-10"></div>
-					<div class="top-tile top-row-1"></div>
-					<div class="top-tile top-row-2"></div>
-					<div class="top-tile top-row-3"></div>
-					<div class="top-tile top-row-4"></div>
-					<div class="top-tile top-row-5"></div>
-					<div class="top-tile top-row-6"></div>
-					<div class="top-tile top-row-7"></div>
-					<div class="top-tile top-row-8"></div>
-					<div class="top-tile top-row-9"></div>
-					<div class="top-tile top-row-10"></div>
-					<div class="right-tile right-row-1"></div>
-					<div class="right-tile right-row-2"></div>
-					<div class="right-tile right-row-3"></div>
-					<div class="right-tile right-row-4"></div>
-					<div class="right-tile right-row-5"></div>
-					<div class="right-tile right-row-6"></div>
-					<div class="right-tile right-row-7"></div>
-					<div class="right-tile right-row-8"></div>
-					<div class="right-tile right-row-9"></div>
-					<div class="right-tile right-row-10"></div>
+					<BoardCenter />
+					<BottomSide />
+					<LeftSide />
+					<TopSide />
+					<RightSide />
 				</div>
 			</div>
 		);
---
+++ src/IncomeTax.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const IncomeTax = () => (
+	<div class="income-tax">
+		<div>Income Tax</div>
+		<div class="icon">
+		</div>
+		<div>(Pay £200)</div>
+	</div>
+
+);
+
+export default IncomeTax 
